92b7cb3e99be2f0120da0c2a66f4f0e0b71d06a6
TheCursedScrolls <shyverazul@gmail.com>
2020-09-04 13:55:50 +0200

    First commit

---
+++ precache-manifest.727cf41c5fefa3ece870789b6e7db7c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ba7177d8dd9e64da73a0d6f27f0cd8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e715bdcde9a1787c063",
+    "url": "./static/css/main.58ea3b00.chunk.css"
+  },
+  {
+    "revision": "c8b95f857e93b1703764",
+    "url": "./static/js/2.a933b42a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.a933b42a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e715bdcde9a1787c063",
+    "url": "./static/js/main.aeff059b.chunk.js"
+  },
+  {
+    "revision": "4bef277201cb1ed0dc0a",
+    "url": "./static/js/runtime-main.d8d6a94b.js"
+  },
+  {
+    "revision": "4554c00133a06b1722232a55335bca32",
+    "url": "./static/media/oncology_white.4554c001.png"
+  }
+]);
